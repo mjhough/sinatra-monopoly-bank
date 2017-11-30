6de76fcb2d09e6138a60755ff8a2865e581f4343
Matt Hough <matt@hough.tv>
2017-11-30 21:39:03 +1000

    Add show property view.

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -6,15 +6,15 @@ class PropertiesController < ApplicationController
         erb :"properties/index"
     end
 
+    get "/properties/:id" do
+        @users = User.where(game: current_game)
+        @property = Property.find(params[:id])
+        erb :"properties/show"
+    end
+
     get "/properties/:id/payments" do
         @users = User.where(game: current_game)
         @payments = Payment.where(property: Property.find(params[:id]))
         erb :"properties/payments_show"
-    end
-
-    get "/properties/:id/" do
-        @users = User.where(game: current_game)
-        @property = Property.find(params[:id])
-        erb :"properties/show"
     end
 end
